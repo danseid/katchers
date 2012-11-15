298312c0f2c20f50756ab109cc0d09c4e02b5898
Daniel Seidler @Work <daniel.seidler@metro-mgp.com>
2012-11-15 16:20:17 +0100

    defined verb and some verbs

---
+++ src/main/kotlin/spec/matchers/Verb.kt
@@ -0,0 +1,47 @@
+package spec.matchers
+
+/**
+ * User: Daniel Seidler
+ * Date: 15.11.12
+ * Time: 08:40
+ *
+ */
+
+enum class Verb{
+    BE
+    NOTBE
+    HAVE
+    NOTHAVE
+    START
+    NOTSTART
+    END
+    NOTEND
+    CONTAIN
+    NOTCONTAIN
+    MATCH
+    NOTMATCH
+    fun not(): Verb {
+        return when(this){
+            BE -> NOTBE
+            NOTBE -> BE
+            HAVE -> NOTHAVE
+            NOTHAVE -> HAVE
+            START -> NOTSTART
+            NOTSTART -> START
+            END -> NOTEND
+            NOTEND -> END
+            CONTAIN -> NOTCONTAIN
+            NOTCONTAIN -> CONTAIN
+            MATCH -> NOTMATCH
+            NOTMATCH -> MATCH
+            else -> BE
+        }
+    }
+}
+
+val be = Verb.BE
+val have = Verb.HAVE
+val start = Verb.START
+val end = Verb.END
+val contain = Verb.CONTAIN
+val match = Verb.MATCH
---
+++ src/test/kotlin/VerbTest.kt
@@ -0,0 +1,48 @@
+/**.
+ * User: Daniel Seidler
+ * Date: 15.11.12
+ * Time: 10:40
+ */
+
+import kotlin.test.assertEquals
+import org.junit.Test as test
+import spec.matchers.*
+
+public class VerbTest {
+    test fun be() {
+        assertEquals(be, Verb.BE)
+        assertEquals(!be, Verb.NOTBE)
+        assertEquals(!!be, Verb.BE)
+    }
+
+    test fun have() {
+        assertEquals(have, Verb.HAVE)
+        assertEquals(!have, Verb.NOTHAVE)
+        assertEquals(!!have, Verb.HAVE)
+    }
+
+    test fun start() {
+        assertEquals(start, Verb.START)
+        assertEquals(!start, Verb.NOTSTART)
+        assertEquals(!!start, Verb.START)
+    }
+
+    test fun end() {
+        assertEquals(end, Verb.END)
+        assertEquals(!end, Verb.NOTEND)
+        assertEquals(!!end, Verb.END)
+    }
+
+    test fun contain() {
+        assertEquals(contain, Verb.CONTAIN)
+        assertEquals(!contain, Verb.NOTCONTAIN)
+        assertEquals(!!contain, Verb.CONTAIN)
+    }
+
+    test fun match() {
+        assertEquals(match, Verb.MATCH)
+        assertEquals(!match, Verb.NOTMATCH)
+        assertEquals(!!match, Verb.MATCH)
+    }
+
+}
