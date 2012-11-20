907824ed139ea8bef5b071cb71cddda03b3a27f2
Daniel Seidler <daniel.seidler@gmail.com>
2012-11-20 12:11:53 +0100

    License adde into comments

--- src/main/kotlin/spec/matchers/IntMatcher.kt
+++ src/main/kotlin/spec/matchers/IntMatcher.kt
@@ -1,12 +1,25 @@
+/*
+ * Copyright 2012 Daniel Seidler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spec.matchers
 
-/**.
- * User: Daniel Seidler
- * Date: 15.11.12
- * Time: 12:21
+/**
+ * @author Daniel Seidler
+ * @since 2012/11/20
  */
-
-class IntMatcher(target: Int) : Matcher{
+class IntMatcher(target: Int): Matcher{
 
     // greater than: target > value
     fun gt(value: Int) {
@@ -15,7 +28,7 @@ class IntMatcher(target: Int) : Matcher{
 
     // greater than or equal: target > value
     fun gte(value: Int) {
-         greaterOrEqualThan(value)
+        greaterOrEqualThan(value)
     }
 
     // less than: target > value
--- src/main/kotlin/spec/matchers/Verb.kt
+++ src/main/kotlin/spec/matchers/Verb.kt
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2012 Daniel Seidler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spec.matchers
 
 /**
- * User: Daniel Seidler
- * Date: 15.11.12
- * Time: 08:40
- *
+ * @author Daniel Seidler
+ * @since 2012/11/20
+ * Verb: an object that will be passed to a should function: <expression>.should(<verb>)
+ * using an infix notation it will be written like this: <expression> should verb
+ * example: string should be
  */
 trait Verb {
 }
@@ -52,6 +68,7 @@ public class NOTFAIL: Verb {
     fun not() = fail
 }
 
+/* VALUES */
 val be = BE()
 val notbe = NOTBE()
 val have = HAVE()
